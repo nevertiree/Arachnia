884b9b87447c497c679887b97ce29b42a5cbf4f1
nevertiree <nevertiree@126.com>
2016-12-29 10:52:31 +0800

    Add Book info test

--- DoubanBook/src/test/java/com/nevertiree/business/BookFilterTest.java
+++ DoubanBook/src/test/java/com/nevertiree/business/BookFilterTest.java
@@ -11,65 +11,91 @@ import static org.junit.Assert.assertEquals;
 public class BookFilterTest {
 
     private BookFilter bookFilter;
-    private String url_think_in_java = "https://book.douban.com/subject/1474824/";
+
+    private String url = "https://book.douban.com/subject/3291901/";
+    private String exceptedName = "Linux Kernel Development";
+    private String exceptedScore = "9.0";
+    private String exceptedVote = "130";
+    private String exceptedRank5 = "58.5";
+    private String exceptedRank4 = "35.4";
+    private String exceptedRank3 = "6.2";
+    private String exceptedRank2 = "0.0";
+    private String exceptedRank1 = "0.0";
+
+    private BookVO book = new BookVO();
 
     @Before
     public void setUp() throws Exception{
         bookFilter = new BookFilter();
     }
 
+//    @Test
+//    public void testGetBookInfo() throws Exception{
+//        String testWebContent = GetWebContent.getWebContent(url);
+//        book.setName(exceptedName);
+//        book.setScore(Double.parseDouble(exceptedScore));
+//        book.setReaderNum(Integer.parseInt(exceptedVote));
+//        book.setScoreRank1(Double.parseDouble(exceptedRank1));
+//        book.setScoreRank2(Double.parseDouble(exceptedRank2));
+//        book.setScoreRank3(Double.parseDouble(exceptedRank3));
+//        book.setScoreRank4(Double.parseDouble(exceptedRank4));
+//        book.setScoreRank5(Double.parseDouble(exceptedRank5));
+//
+//        assertEquals(book,BookFilter.getBookInfo(url));
+//    }
+
     @Test
     public void testGetBookName() throws Exception{
-        String testWebContent = GetWebContent.getWebConentx(url_think_in_java);
-        assertEquals("Thinking in Java",BookFilter.getBookName(testWebContent));
+        String testWebContent = GetWebContent.getWebContent(url);
+        assertEquals(exceptedName,BookFilter.getBookName(testWebContent));
     }
 
     @Test
     public void testGetBookScore() throws Exception{
-        String testWebContent = GetWebContent.getWebConentx(url_think_in_java);
-        assertEquals("9.0",BookFilter.getBookScore(testWebContent));
+        String testWebContent = GetWebContent.getWebContent(url);
+        assertEquals(exceptedScore,BookFilter.getBookScore(testWebContent));
     }
 
     @Test
     public void testGetVoteNumber() throws Exception{
-        String testWebContent = GetWebContent.getWebConentx(url_think_in_java);
-        assertEquals("456",BookFilter.getVoteNumber(testWebContent));
+        String testWebContent = GetWebContent.getWebContent(url);
+        assertEquals(exceptedVote,BookFilter.getVoteNumber(testWebContent));
     }
 
     @Test
     public void testRank5Rate() throws Exception{
-        String testWebContent = GetWebContent.getWebConentx(url_think_in_java);
-        assertEquals("61.4",BookFilter.getRank5Rate(testWebContent));
+        String testWebContent = GetWebContent.getWebContent(url);
+        assertEquals(exceptedRank5,BookFilter.getRank5Rate(testWebContent));
     }
 
     @Test
     public void testRank4Rate() throws Exception{
-        String testWebContent = GetWebContent.getWebConentx(url_think_in_java);
-        assertEquals("28.3",BookFilter.getRank4Rate(testWebContent));
+        String testWebContent = GetWebContent.getWebContent(url);
+        assertEquals(exceptedRank4,BookFilter.getRank4Rate(testWebContent));
     }
 
     @Test
     public void testRank3Rate() throws Exception{
-        String testWebContent = GetWebContent.getWebConentx(url_think_in_java);
-        assertEquals("8.6",BookFilter.getRank3Rate(testWebContent));
+        String testWebContent = GetWebContent.getWebContent(url);
+        assertEquals(exceptedRank3,BookFilter.getRank3Rate(testWebContent));
     }
 
     @Test
     public void testRank2Rate() throws Exception{
-        String testWebContent = GetWebContent.getWebConentx(url_think_in_java);
-        assertEquals("1.1",BookFilter.getRank2Rate(testWebContent));
+        String testWebContent = GetWebContent.getWebContent(url);
+        assertEquals(exceptedRank2,BookFilter.getRank2Rate(testWebContent));
     }
 
     @Test
     public void testRank1Rate() throws Exception{
-        String testWebContent = GetWebContent.getWebConentx(url_think_in_java);
-        assertEquals("0.7",BookFilter.getRank1Rate(testWebContent));
+        String testWebContent = GetWebContent.getWebContent(url);
+        assertEquals(exceptedRank1,BookFilter.getRank1Rate(testWebContent));
     }
 
-    @Test
-    public void testGetRelativeBook(){
-        String testWebContent = GetWebContent.getWebConentx(url_think_in_java);
-        assertEquals(15,BookFilter.getRelativeBook(testWebContent).size());
-    }
+//    @Test
+//    public void testGetRelativeBook(){
+//        String testWebContent = GetWebContent.getWebContent(url);
+//        assertEquals(15,BookFilter.getRelativeBook(testWebContent).size());
+//    }
 
 }
